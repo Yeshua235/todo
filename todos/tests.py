ca08f6d5b6c75eefcab43e510b16977e72ced588
Joshua Ayanlowo <joshuaayanlowo235@gmail.com>
2024-07-25 16:12:16 +0100

    removed unused imports

--- todos/tests.py
+++ todos/tests.py
@@ -4,7 +4,6 @@ from django.test import TestCase
 # Create your tests here.
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
 
 from .models import Todo
 
--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,5 @@
 # pylint: skip-file
 
-from django.shortcuts import render
-
 # Create your views here.
 from rest_framework import generics
 from .models import Todo
